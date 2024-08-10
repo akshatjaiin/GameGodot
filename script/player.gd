bb7b3b8b7aa6d3e57fe802b7c9510450fe90fef4
Akshat Jain <akshat0@yahoo.com>
2024-08-10 17:57:44 +0530

    platform

--- script/player.gd
+++ script/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -500.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -14,7 +14,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("JUMP") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
