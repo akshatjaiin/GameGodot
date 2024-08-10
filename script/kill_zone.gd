a2dd87980ef348004f21896577f09b96f05164eb
Akshat Jain <akshat0@yahoo.com>
2024-08-10 23:19:41 +0530

    yahh

--- script/kill_zone.gd
+++ script/kill_zone.gd
@@ -6,13 +6,10 @@ extends Area2D
 func _on_body_entered(body):
 	print("you died") # Replace with function body.
 	Engine.time_scale = 0.5
-	body.get_node("CollissionShape2D").queue_free()
+	print(body.get_children())
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
-
-
-
-
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
--- script/slime.gd
+++ script/slime.gd
@@ -4,6 +4,7 @@ var direction = 1
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
 
 const SPEED = 60
 # Called every frame. 'delta' is the elapsed time since the previous frame.
